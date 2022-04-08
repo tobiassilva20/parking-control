ad0cff59cc8ebe92a83a0e318d6b89d3081e0179
Tobias Silva <tobiassilva@hotmail.it>
2022-04-08 13:20:55 -0300

    Primeiro commit

---
+++ parking-control/src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,19 @@
+package com.api.parkingcontrol.repositories;
+
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
